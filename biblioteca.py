f5676c60528b3342079973b8065140ff2576edc2
kiyohiroleo <kiyohiroleo@gmail.com>
2025-04-30 15:36:16 -0300

    checklist a verificar

--- biblioteca.py
+++ biblioteca.py
@@ -4,7 +4,7 @@ class Biblioteca:
     livros_disponiveis = []
     livros_emprestados = []
 
-    def adicionarUsuario(self):
+    def cadastrarUsuario(self):
         nome = input("Qual o seu nome completo? ")
         email = input("E-mail para contato: ")
         limite_livros = input("Gostaria de ser um membro VIP? (S/N) ")
@@ -26,7 +26,6 @@ class Biblioteca:
             else:
                 print(f"Nome: {u.nome} | Email: {u.email} Livros: {u.lista_de_livros} Membro: {"Regular", u.membro, "de limite"}")
 
-
     def exibirLivrosEmprestados(self):
         for l in self.livros_emprestados:
             print(f"Livro: {l.titulo} | Autor {l.autor} | Ano {l.ano} | Status {l.status}")
@@ -35,29 +34,11 @@ class Biblioteca:
         for l in self.livros_disponiveis:
             print(f"Livro: {l.titulo} | Autor {l.autor} | Ano {l.ano} | Status {l.status}")
 
-    def adicionarLivro(self):
+    def alugarLivro(self):
+        pass
 
-        nome = input("Qual o seu nome completo? ")
-
-        if any(u.nome.lower() == nome.lower() for u in self.usuarios_cadastrados):
-            titulo = input("Qual o titulo do Livro? ")
-            autor = input("Qual o autor do Livro? ")
-            ano = input("Qual o ano do Livro? ")
-            status = input("Gostaria de uma versão física ou digital do Livro? ")
-            
-            livro = Livro(titulo, autor, ano, status)
-            self.livros_emprestados.append(livro)
-
-            if status.lower() == "digital":
-                status = LivroDigital(titulo, autor, ano, status)
-            else:
-                status = LivroFisico(titulo, autor, ano, status)
-
-            print("Usuário encontrado, prossiga com o cadastro do livro.")
-
-            print("Livro cadastrado com sucesso.")
-        else:
-            print("Usuário ainda não cadastrado.")
+    def devolver(self):
+        pass
 
     def buscarLivro(self):
 
@@ -68,6 +49,8 @@ class Biblioteca:
         else:
             print("Livro indisponível.")
 
+    
+            
 # Livro
 class Livro:
     def __init__(self, titulo, autor, ano, status):
@@ -111,26 +94,31 @@ def interface():
         print("2. Cadastrar Livro.")
         print("3. Exibir Usuários")
         print("4. Exibir Livros Disponiveis")
-        print("5. Exibir Livros Disponiveis")
+        print("5. Exibir Livros Emprestados")
         print("6. Buscar Livros")
-        print("7. Sair")
+        print("7. Devolver")
+        print("8. Sair")
 
         escolha = input("Escolha uma das opções: ")
         if escolha == "1":
-            biblioteca.adicionarUsuario()
+            biblioteca.cadastrarUsuario()
         elif escolha == "2":
-            biblioteca.adicionarLivro()
+            biblioteca.alugarLivro()
         elif escolha == "3":
             biblioteca.exibirUsuarios()
         elif escolha == "4":
-            biblioteca.exibirLivrosEmprestados()
-        elif escolha == "5":
             biblioteca.exibirLivrosDisponiveis()
+        elif escolha == "5":
+            biblioteca.exibirLivrosEmprestados()
         elif escolha == "6":
             biblioteca.buscarLivro()
         elif escolha == "7":
+            biblioteca.devolver()
+        elif escolha == "8":
             break
         else:
             print("Escolha Inválida.")
 
 interface()
+
+# Checklist: A verificar
